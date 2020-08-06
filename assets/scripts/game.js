a7903648043507fadec678f340eb5fdfc3e623b2
Robert <bonjour_robert@outlook.com>
2020-08-06 14:48:53 +0200

    initial commit

---
+++ assets/scripts/game.js
@@ -0,0 +1,4 @@
+// List of all cards icons
+var Cards = ["百","千","万","上","中","下","左","右","大","小","百","千","万","上","中","下","左","右","大","小"];
+// An empty array where values of open cards are pushed to compare.
+var OpenCards= [];
