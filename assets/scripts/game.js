0c6d4916ec9cc973626e2aaba2fcf16af535cef5
Robert <bonjour_robert@outlook.com>
2020-08-07 21:50:08 +0200

    tests array returned

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,4 +1,9 @@
-// List of all cards icons
+// List of all cards by character
 var Cards = ["百","千","万","上","中","下","左","右","大","小","百","千","万","上","中","下","左","右","大","小"];
 // An empty array where values of open cards are pushed to compare.
 var OpenCards= [];
+//An empty array where shuffled cards are stored.
+var shuffleCards=[];
+var tilesFlipped=0;
+var numOfmoves=0;
+console.log(Cards)
